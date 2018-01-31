6ebc12bbbf225b320b6dc8b50118a260c0612f81
Cindy <cl4208@nyu.edu>
2018-01-31 14:08:38 -0500

    Changed gitignore

---
+++ Game Dev Project 3D/Assets/movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    float xSpeed = 5f;
+    float ySpeed = 10f;
+
+    Rigidbody myRigidbody;
+
+    private bool isJumping = false;
+    private bool isMoving = false;
+
+    public GameObject carrotSprite;
+
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.velocity = new Vector2(-xSpeed, myRigidbody.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.velocity = new Vector2(xSpeed, myRigidbody.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping != true)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, ySpeed);
+            isJumping = true;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GameObject newknifeSpriteObj = Instantiate(carrotSprite);
+            newknifeSpriteObj.transform.position = new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z - 1f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(0, 1.7f, 2.55f);
+        }
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "floor")
+        {
+            isJumping = false;
+        }
+    }
+
+}
