665ea6a79b601c854bc1918c5a524706cd0266c0
Cindy <cl4208@nyu.edu>
2018-02-07 14:58:40 -0500

    Set up systems to switch between boss attacks

--- Game Dev Project 3D/Assets/boss.cs
+++ Game Dev Project 3D/Assets/boss.cs
@@ -6,9 +6,17 @@ using UnityEngine.SceneManagement;
 public class boss : MonoBehaviour
 {
     public GameObject eye;
-    public int health = 20;
+    public GameObject heart;
+
+    public int health = 50;
+
     SpriteRenderer mySpriteRenderer;
-    float timer = 1f;
+
+    float pelletTimer = 1f;
+    float waveTimer = 3f;
+    public float attackCounterTimer = 5f;
+
+    public int attackCounter;
 
     // Use this for initialization
     void Start()
@@ -19,13 +27,36 @@ public class boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer <= 0)
+        pelletTimer -= Time.deltaTime;
+        waveTimer -= Time.deltaTime;
+        attackCounterTimer -= Time.deltaTime;
+
+        if (attackCounterTimer <= 0)
         {
-            GameObject newbossAttack = Instantiate(eye);
-            newbossAttack.transform.position = new Vector3(Random.Range(6f, 7f), Random.Range(-1f, 1f), .03f);
-            timer = 1f;
+            attackCounter = Random.Range(1, 5);
+            attackCounterTimer = 5f;
         }
+        
+        if (attackCounter == 1)
+        {
+            if (pelletTimer <= 0)
+            {
+                GameObject newbossAttack = Instantiate(eye);
+                newbossAttack.transform.position = new Vector3(Random.Range(6f, 7f), Random.Range(-1f, 1f), .03f);
+                pelletTimer = 1f;
+            }
+        }
+        if (attackCounter == 2)
+        {
+            if (waveTimer <= 0)
+            {
+                GameObject newbossAttack = Instantiate(heart);
+                newbossAttack.transform.position = new Vector3(transform.position.x, transform.position.y, .03f);
+                waveTimer = 3f;
+            }
+           
+        }
+        
         if (health <= 0)
         {
             Destroy(gameObject);
